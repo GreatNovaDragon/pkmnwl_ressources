14b72346b981abd6d9abf890c88572d5db753ffd
Sebastien Bürky <buerkyb@gmail.com>
2023-09-20 16:33:36 +0200

    Adding scaffolded items

--- webapp/pkmnWildLife/Pages/pokemon/Details.cshtml.cs
+++ webapp/pkmnWildLife/Pages/pokemon/Details.cshtml.cs
@@ -1,42 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using pkmnWildLife.Data;
 
-namespace pkmnWildLife.Pages.pokemon
+namespace pkmnWildLife.Pages.pokemon;
+
+public class DetailsModel : PageModel
 {
-    public class DetailsModel : PageModel
+    private readonly ApplicationDbContext _context;
+
+    public DetailsModel(ApplicationDbContext context)
     {
-        private readonly pkmnWildLife.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public DetailsModel(pkmnWildLife.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public Pokemon Pokemon { get; set; } = default!;
 
-      public Pokemon Pokemon { get; set; } = default!; 
+    public async Task<IActionResult> OnGetAsync(string id)
+    {
+        if (id == null || _context.Pokedex == null) return NotFound();
 
-        public async Task<IActionResult> OnGetAsync(string id)
-        {
-            if (id == null || _context.Pokedex == null)
-            {
-                return NotFound();
-            }
-
-            var pokemon = await _context.Pokedex.FirstOrDefaultAsync(m => m.ID == id);
-            if (pokemon == null)
-            {
-                return NotFound();
-            }
-            else 
-            {
-                Pokemon = pokemon;
-            }
-            return Page();
-        }
+        var pokemon = await _context.Pokedex.FirstOrDefaultAsync(m => m.ID == id);
+        if (pokemon == null)
+            return NotFound();
+        Pokemon = pokemon;
+        return Page();
     }
 }
