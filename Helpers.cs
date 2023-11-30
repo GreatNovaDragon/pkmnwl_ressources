af100b91a918b61a80711cfa27789b2277844c82
Sebastien Bürky <buerkyb@gmail.com>
2023-11-30 04:39:04 +0100

    Layout update. basically the colors update

--- Helpers.cs
+++ Helpers.cs
@@ -28,4 +28,30 @@ public class Helpers
         [Index(1)] public string effect { get; set; }
         [Index(2)] public string Requirement { get; set; }
     }
+
+    public static string TypeToColor(Data.Type type)
+    {
+        return type.ID switch
+        {
+            "bug" => "#91A119",
+            "dark" => "#50413f",
+            "dragon" => "#5060e0",
+            "electric" => "#f8bf00",
+            "fire" => "#e52829",
+            "fairy" => "#ed6fed",
+            "fighting" => "#fd8f00",
+            "flying" => "#a1bbec",
+            "ghost" => "#6f416f",
+            "grass" => "#3fa029",
+            "ground" => "#905121",
+            "ice" => "#3fd7fd",
+            "normal" => "#9ea09e",
+            "poison" => "#9041ca",
+            "psychic" => "#ed4178",
+            "rock" => "#aea880",
+            "steel" => "#60a0b7",
+            "water" => "#297fed",
+            _ => "#000000"
+        };
+    }
 }
